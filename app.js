7f024219c51db9b9b3c4a485738a475cedd19eb3
MegaPet <90146700+MegaPet@users.noreply.github.com>
2021-11-10 00:19:56 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 function DarkTheme(){
     var checkCondition = document.getElementById('changebox');
     var r = document.querySelector(':root');
@@ -31,3 +32,25 @@ function DarkTheme(){
 function buttonNotWorking(){
     window.alert('Nem értek még hozzá, sajnálom nem fog menni, pusszantás!')
 }
+
+jQuery(window).resize(function () {
+    var r = document.querySelector(':root');
+    var windowWidth = jQuery(document).width();
+    if(windowWidth > 1510){
+        r.style.setProperty('--nav-links', '2.5rem')
+        r.style.setProperty('--h1', '3.5rem')
+        r.style.setProperty('--paragraph', '1.8rem')
+        r.style.setProperty('--small', '1.5rem')
+    }else if(windowWidth < 1510){
+        r.style.setProperty('--nav-links', '2rem')
+        r.style.setProperty('--h1', '3.3rem')
+        r.style.setProperty('--paragraph', '1.5rem')
+        r.style.setProperty('--small', '1.2rem')
+    }else if(windowWidth > 1300){
+        console.log('windows width : '+ windowWidth);
+        r.style.setProperty('--nav-links', '1.3rem')
+        r.style.setProperty('--h1', '2rem')
+        r.style.setProperty('--paragraph', '1rem')
+        r.style.setProperty('--small', '1.2rem')
+    }
+}) 
